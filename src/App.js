790fd24658ed2014e52feb39b5af43dfa3819729
Shubham Sahu <ss.cse1001@gmail.com>
2024-02-19 23:30:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [initialAmount, setInitialAmount] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [calData, setCalData] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const initialAmountValue = form.elements[0].value;
+    const totalAmountValue = form.elements[1].value;
+    setInitialAmount(initialAmountValue);
+    setTotalAmount(totalAmountValue);
+  };
+
+  const calculateRows = () => {
+    let x = totalAmount;
+    let total = x;
+    let ini = initialAmount;
+    let rows = [];
+    for (let i = 0; i < 100000000; i++) {
+      const xIni = x - ini;
+      const remainingValue = totalAmount - xIni;
+      rows.push({ total, ini, xIni, remainingValue });
+      if (xIni < 0) break;
+      total = x - ini;
+      ini *= 2;
+      x -= ini;
+    }
+    setCalData(rows);
+  };
+
+  useEffect(() => {
+    calculateRows();
+  }, [initialAmount, totalAmount]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="head">Martingale Strategy:</div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Initial Amount:</label>
+          <input type="number" placeholder="Enter initial amount" />
+        </div>
+        <div>
+          <label>Total Amount:</label>
+          <input type="number" placeholder="Enter total amount" />
+        </div>
+        <button type="submit">Calculate</button>
+      </form>
+      <div>
+        <h2>Calculated Data:</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Total</th>
+              <th>Bet</th>
+              <th>Value</th>
+              <th>Total Minus</th>
+            </tr>
+          </thead>
+          <tbody>
+            {calData.map((row, index) => (
+              <tr key={index}>
+                <td>{row.total}</td>
+                <td>{row.ini}</td>
+                <td>{row.xIni}</td>
+                <td>{row.remainingValue}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
